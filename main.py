bee6ceca98fc3f0772d1c63c77d33198f1295a81
Ramiro Gómez <web@ramiro.org>
2009-03-25 13:19:11 +0100

    Added functionality to display user profiles on the site.

--- main.py
+++ main.py
@@ -7,9 +7,21 @@ import twitter
 class MainPage(webapp.RequestHandler):
   def get(self):
     self.response.headers['Content-Type'] = 'text/html'
-    result = get_data(self.request)
-    if result:
-      html = template.render('templates/index.html', dict(result = result))
+    tweets = get_data(self.request)
+    if tweets:
+      html = template.render('templates/index.html', dict(tweets = tweets))
+    else:
+      html = template.render('templates/error.html', {})
+    self.response.out.write(html)
+
+class ProfilePage(webapp.RequestHandler):
+  def get(self):
+    self.response.headers['Content-Type'] = 'text/html'
+    screen_name = self.request.path.split('/')[-1]
+    profile = twitter.get_profile(screen_name)
+    tweets = twitter.user_tweets(screen_name)
+    if profile and tweets:
+      html = template.render('templates/profile.html', dict(profile = profile, tweets = tweets))
     else:
       html = template.render('templates/error.html', {})
     self.response.out.write(html)
@@ -17,9 +29,9 @@ class MainPage(webapp.RequestHandler):
 class JsPage(webapp.RequestHandler):
   def get(self):
     self.response.headers['Content-Type'] = 'text/javascript'
-    result = get_data(self.request)
+    tweets = get_data(self.request)
     if result:
-      html = template.render('templates/tweets.html', dict(result = result))
+      html = template.render('templates/tweets.html', dict(tweets = tweets))
       self.response.out.write(html)
 
 def get_data(request):
@@ -38,12 +50,13 @@ def get_data(request):
       return False
 
   request_params = {'page': page, 'q': q}
-  result = twitter.get_tweets(**request_params)
+  result = twitter.search_tweets(**request_params)
   return result
 
 def main():
   application = webapp.WSGIApplication([
     ('/', MainPage),
+    ('/profile/\w+', ProfilePage),
     ('/tweets', JsPage)
     ],
     debug=True
--- twitter.py
+++ twitter.py
@@ -3,7 +3,7 @@ from google.appengine.api import memcache
 from django.utils import simplejson
 import urllib
 
-def get_tweets(**request_params):
+def search_tweets(**request_params):
   url = u'http://search.twitter.com/search.json?%s'
   default_params = { 'q': '#linux', 'rpp': 10 }
   default_params.update(request_params)
@@ -29,6 +29,26 @@ def get_tweets(**request_params):
         memcache.set(cache_id, result, 300)
         return result
     except:
+      return False
+
+def user_tweets(screen_name):
+  url = u'http://twitter.com/statuses/user_timeline.json?%s'
+  params = {'screen_name': screen_name}
+  return api_request(url, **params)
+  
+def get_profile(screen_name):
+  url = u'http://twitter.com/users/show.json?%s'
+  params = {'screen_name': screen_name}
+  return api_request(url, **params)
+
+def api_request(url, **params):
+  request_url = url % urllib.urlencode(params)
+  try:
+    result = urlfetch.fetch(request_url)
+    if result.status_code == 200:
+      result = simplejson.loads(result.content)
+      return result
+  except:
       return False
 
 if __name__ == "__main__":
